ba9d03fa74b9ea5328c2c919f201226693f407b3
seyoung5744 <53173850+seyoung5744@users.noreply.github.com>
2019-12-10 17:58:02 +0900

    Chain_of_Responsibility
    Chain_of_Responsibility

---
+++ Chain_of_Responsibility/src/test/StdoutLogger.java
@@ -0,0 +1,11 @@
+package test;
+
+public class StdoutLogger extends Logger {
+	public StdoutLogger(int mask) {
+		this.mask = mask;
+	}
+	
+	protected void writeMessage(String msg) {
+		System.out.println("Writing to stdout: " + msg);
+	}
+}
